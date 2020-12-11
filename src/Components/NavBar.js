0a516159b9fd524ab254596dc1648cdb2324719c
Melindadiaz07 <melindadiaz@me.com>
2020-12-10 21:21:53 -0500

    Installed react router dom, updated ui to semantic, added navbar layout

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,59 @@
-import React from 'react';
+
+import React, { Fragment } from "react";
+import { Menu } from "semantic-ui-react";
+import { NavLink, withRouter, Redirect, Switch, useHistory } from "react-router-dom";
+
+
 
 const NavBar = () => {
 
+
+
   return (
     <div>
-
-    </div>
-
+      
+    <Menu pointing secondary>
+   
+    
+      <Fragment>
+        
+        <Menu.Item
+            as={NavLink}
+            to="/home"
+            name="Home"
+            active={pathname === "/home"}
+          />
+        <Menu.Item
+          as={NavLink}
+          to="/about"
+          name="About"
+          active={pathname === "/about"}
+        />
+        <Menu.Item
+          as={NavLink}
+          to="/admissions"
+          name="Admissions"
+          active={pathname === "/admissions"}
+        />
+         <Menu.Item
+          as={NavLink}
+          to="/academics"
+          name="Academics"
+          active={pathname === "/academics"}
+        />
+         <Menu.Item
+          as={NavLink}
+          to="/faculty"
+          name="Faculty"
+          active={pathname === "/faculty"}
+        />
+        
+        </Menu.Menu>
+        
+      </Fragment>
+  
+  </Menu></div>
   )
 }
 
-export default NavBar;
+export default withRouter(NavBar)
