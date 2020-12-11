6e00e065b0ab8bdfb8bdf808913661a59131e90f
Melindadiaz07 <melindadiaz@me.com>
2020-12-11 13:48:16 -0500

    Started setting up contact page links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
 
+    <h3>email: <a href = "mailto:melindadiaz@me.com" >melindadiaz@me.com</a></h3>
+    <h3>LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/melindadiaz/">Melinda Diaz</a></h3>
+    
+
+      
     </div>
   )
 }
