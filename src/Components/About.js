a141fa4054078b2d9570e7996d2b2a54b5b9ef4a
Melindadiaz07 <melindadiaz@me.com>
2020-12-12 13:03:06 -0500

    Added content to about section, set up basic formatting

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-
+    <div className="about-container" >
+     <h3>I'm a silversmith turned developer, who fell in love with coding after building my company’s website. Experienced in Ruby on Rails and JavaScript based programming, with a background in design. With a background in running a business, I know how to prioritize competing tasks under pressure, meet deadlines, and deliver a quality product for the customer.</h3> 
     </div>
   )
 }
