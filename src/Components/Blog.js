9a0194fd5437987e61d07698226833d853a3cd0e
Melindadiaz07 <melindadiaz@me.com>
2020-12-13 23:36:26 -0500

    Installed material ui, set up blog and project components

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,30 +1,15 @@
 
 import React, { useEffect, useState } from 'react';
-import BlogCard from './BlogCard'
+// import BlogCard from './BlogCard'
 
 
 const Blog = () => {
 
-const mediumFeed = "https://medium.com/feed/@melindadiaz-75942"
-const jsonEndpoint = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40melindadiaz-75942"
-const [feed, setFeed] = useState([])
-
-useEffect(() => {
-  fetch(jsonEndpoint)
-  .then(res => res.json())
-  .then(data => {
-    setFeed(data)
-    console.log(data.items)
-  })
-
-}, [])
  
    return(
      <div className="blog-container">
-       { !feed.items ? null : 
-       feed.items.map(blog => {
-         return <BlogCard blog={blog} key={blog.pubDate}/>
-       })}
+       {/* screen-shot top of medium articles, copy text, and create link at bottom 
+       to visit full article on medium */}
      </div>
     );
   
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Project = () => {
+
+
+
+const Projects = () => {
   return (
     <div>
-
+      <div className="side-tabs">
+        {/* add image and short description for each icon tab.
+        create a slected class to highlight the project they've clicked on
+        conditionally render project component based on which one is selected (state) */}
+      </div>
     </div>
   )
 }
 
-export default Project;
+export default Projects;
