96f7f0c1d9686913b2e2bc7100c068b965f0cf0e
Melindadiaz07 <melindadiaz@me.com>
2020-12-15 16:14:30 -0500

    Added icons to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,11 @@
 import React, { useState, } from 'react';
 import ContactFrom from './ContactForm'
+import github from '../images/githublogo.png'
+import email from '../images/email.png'
+import medium from '../images/medium.png'
+import linkedin from '../images/linkedin.png'
+
+
 
 const Contact = () => {
 
@@ -8,10 +14,10 @@ const Contact = () => {
     <div id="contact-page">
     <div className="contact-container">
 
-    <h3>email: <a href = "mailto:melindadiaz@me.com" > melindadiaz@me.com</a></h3>
-    <h3>linkedIn: <a target="_blank" href="https://www.linkedin.com/in/melindadiaz/"> linkedin.com/in/melindadiaz/</a></h3>
-    <h3>gitHub: <a href="https://github.com/melindadiaz07" > github.com/melindadiaz07</a></h3>
-    <h3>blog: <a href="https://melindadiaz-75942.medium.com/" > melindadiaz-75942.medium.com/</a></h3>
+    <h3><img src={email} alt="email logo" className="contact-icons" />email: <a href = "mailto:melindadiaz@me.com" > melindadiaz@me.com</a></h3>
+    <h3><img src={linkedin} alt="linkedin logo" className="contact-icons" />linkedIn: <a target="_blank" href="https://www.linkedin.com/in/melindadiaz/"> linkedin.com/in/melindadiaz/</a></h3>
+    <h3><img src={github} alt="github logo" className="contact-icons" />gitHub: <a href="https://github.com/melindadiaz07" > github.com/melindadiaz07</a></h3>
+    <h3><img src={medium} alt="medium logo" className="contact-icons" />blog: <a href="https://melindadiaz-75942.medium.com/" > melindadiaz-75942.medium.com/</a></h3>
     <br /> <br />
 
     {/* <hr /> */}
