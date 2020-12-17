83dfb6b65edf49fdbe13309075aef46d8f36c1d9
Melindadiaz07 <melindadiaz@me.com>
2020-12-17 13:08:06 -0500

    Updated background and aria styling

--- src/Components/BrakebillsProject.js
+++ src/Components/BrakebillsProject.js
@@ -20,7 +20,7 @@ const BrakebillsProject = () => {
     <h3 className="brakebills-description">
       This project was a fun take on a classic academic website - it's based on a fictional university from the book/
       tv series The Magicians. The premise is that to outsiders, Brakebills is a regular ivy leage school,
-      when in reality it teaches magic (think Hogwarts for adults.</h3>
+      when in reality it teaches magic (think Hogwarts for adults.)</h3>
      
         <h3 className="brakebills-description">
           This concept is reflected in the layout of the website - there's a public facing version that shows
@@ -29,7 +29,7 @@ const BrakebillsProject = () => {
 
       <h3 className="brakebills-description">
         I also had a lot of fun building a message board on the logged-in side. Users can see messages posted by other students,
-        and replies are conditionally rendered by clicking on different posts. The user can also create new posts or replies,
+        and replies are conditionally rendered by clicking on different posts. The user can also add their own posts or replies to the board,
         and the content is instantly rendered to the page as well as sent to the database to persist the information.
       </h3>
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import headshot from '../images/headshot.jpg'
+import headshot from '../images/hexheadshot.png'
 
 const Home = () => {
 
   return(
     <div id="home-page">
-    <div className="home-page-container">
-      <img src={headshot} alt="headshot" className="headshot"/>
+  
+
+<div className="home-page-container">
+<hr id="top-rule"></hr>
+  <img src={headshot} alt="headshot" className="headshot"/>
+  <hr id="bottom-rule"></hr>
       <h2>
         Hi, I'm Melinda Diaz - a Software Engineer and Full Stack Developer living in Alexandria, VA.
       </h2>
     </div>
+ <div className="text-background"></div>
     </div>
 
   )
