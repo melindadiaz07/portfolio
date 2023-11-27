deb893721c341d4ea54a9abb10281487f8cc5a2e
Melinda Diaz <melindadiaz@me.com>
2023-11-26 17:54:46 -0800

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
   <img src={headshot} alt="headshot" className="headshot"/>
   <hr id="bottom-rule"></hr>
       <h2>
-        Hi, I'm Melinda Diaz - a Software Engineer and Full Stack Developer living in Alexandria, VA.
+        Hi, I'm Melinda Diaz - a Software Engineer and Full Stack Developer living in Eugene, OR.
       </h2>
     </div>
  <div className="text-background"></div>
