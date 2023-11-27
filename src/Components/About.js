173d7caef1ff6fbffb7397aeafa9e10aed817625
Melinda Diaz <melindadiaz@me.com>
2023-11-26 17:58:32 -0800

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ const About = () => {
 <br /><br />
    As a designer, I was initially drawn to front-end development (HTML/CSS and Javascript), but after learning Ruby and creating my own Rails-based API I quickly found backend development just as rewarding - I’m constantly amazed at how something as seemingly logic-based as writing code actually involves a tremendous amount of creativity. It’s the perfect blend of right and left brain problem solving, and I can't get enough of learning new ways to work with different languages and frameworks.
 <br /><br />
-   I have experience working with Javascript, HTML/CSS, React, Redux, Vue, React Native, Typescript, Ruby/Rails, SQL, PostgreSQL, Bootstrap, and REST APIs. I’m very interested in working with and learning design principles to keep my code well-organized, like the MVC design pattern. As a lifelong learner, I always have a new language (or 3) I’m learning - currently I’m studying Python and C#.</h3> 
+   I have experience working with Javascript, HTML/CSS, React, Redux, Vue, React Native, Typescript, Ruby/Rails, SQL, PostgreSQL, Bootstrap, and REST APIs. I’m very interested in working with and learning design principles to keep my code well-organized, like the MVC design pattern. </h3> 
     </div>
     </div>
   )
